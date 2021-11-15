44723e9be66a3bcbd20f9746e2dab5b409c2bd5d
rartigues2019 <rartigues2019@inf.udec.cl>
2021-11-15 03:29:56 -0300

    listo register

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React,{Fragment, useState} from 'react'
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+//Components
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  const setAuth= boolean => {
+    setIsAuthenticated(boolean);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) => !isAuthenticated ? (<Login {...props} setAuth={setAuth}/>) : (<Redirect to="/dashboard" />)}
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) => !isAuthenticated ? (<Register {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />)}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) => isAuthenticated ? (<Dashboard {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />)}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
+
 
 export default App;
